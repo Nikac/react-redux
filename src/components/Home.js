d39710dec11670bb5c876e89b390cb237e2d9ac2
Nikola <nikolaguzina@yahoo.com>
2020-07-22 13:02:12 +0200

    small change

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = (props) => {
-  return <h1>Welcome to Home Page</h1>;
+  return (
+    <>
+      <h1>Welcome to Home Page</h1>
+    </>
+  );
 };
 
 export default Home;
